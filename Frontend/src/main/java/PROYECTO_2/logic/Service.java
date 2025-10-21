491b204f379b76051717dc7936d7a25f72a37e5c
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2025-10-20 22:52:44 -0600

    feat(service): implement the methods

--- Frontend/src/main/java/PROYECTO_2/logic/Service.java
+++ Frontend/src/main/java/PROYECTO_2/logic/Service.java
@@ -47,142 +47,374 @@ public class Service {
     // ================= Medicos =================
 
     public void create(Medico e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.MEDICO_CREATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() == Protocol.ERROR_NO_ERROR){}
+        else throw new Exception("Medico ya existe");
     }
 
     public Medico read(Medico e) throws Exception {
-        return null; // Implementar
+        os.writeInt(Protocol.MEDICO_READ);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() == Protocol.ERROR_NO_ERROR){
+            return (Medico) is.readObject();
+        }
+        else throw new Exception("Medico no existe");
     }
 
     public void update(Medico e) throws Exception{
-        // Implementar
+        os.writeInt(Protocol.MEDICO_UPDATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() == Protocol.ERROR_NO_ERROR){}
+        else throw new Exception("Medico no existe");
     }
 
     public void removeMedico(Medico e) throws Exception{
-        // Implementar
+        os.writeInt(Protocol.MEDICO_DELETE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() == Protocol.ERROR_NO_ERROR){}
+        else throw new Exception("Medico no existe");
     }
 
     public List<Medico> findAll(){
-        return new ArrayList<Medico>(); // Implementar
+        try{
+            os.writeInt(Protocol.MEDICO_FIND_ALL);
+            os.flush();
+            if(is.readInt() == Protocol.ERROR_NO_ERROR){
+                return (List<Medico>) is.readObject();
+            }
+            else return List.of();
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Medico> filtrarMedicos(String tipo, String texto) {
-        return new ArrayList<Medico>(); // Implementar
+        try{
+            os.writeInt(Protocol.MEDICO_FILTRAR);
+            os.writeObject(tipo);
+            os.writeObject(texto);
+            os.flush();
+            if(is.readInt() == Protocol.ERROR_NO_ERROR){
+                return (List<Medico>) is.readObject();
+            }
+            else return List.of();
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     // ================= Farmaceuta =================
 
     public void create(Farmaceuta e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.FARMACEUTA_CREATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Farmaceuta ya existe");
+        }
     }
 
     public void update(Farmaceuta e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.FARMACEUTA_UPDATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Farmaceuta no existe");
+        }
     }
 
     public void removeFarmaceuta(Farmaceuta e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.FARMACEUTA_DELETE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Farmaceuta no existe");
+        }
     }
 
-    public List<Farmaceuta> findAllFarmaceutas(){
-        return new ArrayList<Farmaceuta>(); // Implementar
+    public List<Farmaceuta> findAllFarmaceutas() {
+        try {
+            os.writeInt(Protocol.FARMACEUTA_FIND_ALL);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Farmaceuta>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Farmaceuta> filtrarFarmaceutas(String tipo, String texto) {
-        return new ArrayList<Farmaceuta>(); // Implementar
+        try {
+            os.writeInt(Protocol.FARMACEUTA_FILTRAR);
+            os.writeObject(tipo);
+            os.writeObject(texto);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Farmaceuta>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // ================= Pacientes =================
 
     public void create(Paciente e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.PACIENTE_CREATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Paciente ya existe");
+        }
     }
 
     public void update(Paciente e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.PACIENTE_UPDATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Paciente no existe");
+        }
     }
 
     public void removePaciente(Paciente e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.PACIENTE_DELETE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Paciente no existe");
+        }
     }
 
     public List<Paciente> getListaPacientes() {
-        return new ArrayList<Paciente>(); // Implementar
+        try {
+            os.writeInt(Protocol.PACIENTE_FIND_ALL);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Paciente>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Paciente> filtrarPacientes(String tipo, String texto) {
-        return new ArrayList<Paciente>(); // Implementar
+        try {
+            os.writeInt(Protocol.PACIENTE_FILTRAR);
+            os.writeObject(tipo);
+            os.writeObject(texto);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Paciente>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // ================= Medicamento =================
 
     public void create(Medicamento e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.MEDICAMENTO_CREATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Medicamento ya existe");
+        }
     }
 
     public void update(Medicamento e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.MEDICAMENTO_UPDATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Medicamento no existe");
+        }
     }
 
     public void removeMedicamento(Medicamento e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.MEDICAMENTO_DELETE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Medicamento no existe");
+        }
     }
 
     public List<Medicamento> getListaMedicamentos() {
-        return new ArrayList<Medicamento>(); // Implementar
+        try {
+            os.writeInt(Protocol.MEDICAMENTO_FIND_ALL);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Medicamento>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Medicamento> filtrarMedicamentos(String tipo, String texto) {
-        return new ArrayList<Medicamento>(); // Implementar
+        try {
+            os.writeInt(Protocol.MEDICAMENTO_FILTRAR);
+            os.writeObject(tipo);
+            os.writeObject(texto);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Medicamento>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // ================= Recetas =================
 
     public void create(Receta e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.RECETA_CREATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Receta ya existe");
+        }
     }
 
     public void update(Receta e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.RECETA_UPDATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Receta no existe");
+        }
     }
 
     public List<Receta> getListaRecetas() {
-        return new ArrayList<Receta>(); // Implementar
+        try {
+            os.writeInt(Protocol.RECETA_FIND_ALL);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Receta>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Receta> filtrarRecetas(String tipo, String texto) {
-        return new ArrayList<Receta>(); // Implementar
+        try {
+            os.writeInt(Protocol.RECETA_FILTRAR);
+            os.writeObject(tipo);
+            os.writeObject(texto);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Receta>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public List<Receta> filtrarRecetas(String tipo, String texto1, String texto2){
-        return new ArrayList<Receta>(); // Implementar
+    public List<Receta> filtrarRecetas(String tipo, String texto1, String texto2) {
+        try {
+            os.writeInt(Protocol.RECETA_FILTRAR); // si tienes un opcode específico, cámbialo aquí
+            os.writeObject(tipo);
+            os.writeObject(texto1);
+            os.writeObject(texto2);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Receta>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // ================= Prescripciones =================
 
     public void create(Prescripcion e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.PRESCRIPCION_CREATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("No se pudo crear prescripción");
+        }
     }
 
     // ================= Usuario =================
 
-    public Medico getUsuario() throws Exception{
-        return null; // Implementar
+    public Medico getUsuario() throws Exception {
+        os.writeInt(Protocol.USUARIO_GET);
+        os.flush();
+        if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+            return (Medico) is.readObject();
+        } else {
+            throw new Exception("Usuario no existe");
+        }
     }
 
     public void create(Usuario e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.USUARIO_CREATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Usuario ya existe");
+        }
     }
 
     public Usuario read(Usuario e) throws Exception {
-        return null; // Implementar
+        os.writeInt(Protocol.USUARIO_READ);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+            return (Usuario) is.readObject();
+        } else {
+            throw new Exception("Usuario no existe");
+        }
     }
 
     public void update(Usuario e) throws Exception {
-        // Implementar
+        os.writeInt(Protocol.USUARIO_UPDATE);
+        os.writeObject(e);
+        os.flush();
+        if (is.readInt() != Protocol.ERROR_NO_ERROR) {
+            throw new Exception("Usuario no existe");
+        }
     }
 
     public List<Usuario> getListaUsuarios() {
-        return new ArrayList<Usuario>(); // Implementar
+        try {
+            os.writeInt(Protocol.USUARIO_FIND_ALL);
+            os.flush();
+            if (is.readInt() == Protocol.ERROR_NO_ERROR) {
+                return (List<Usuario>) is.readObject();
+            } else {
+                return List.of();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
